b4dbcb88776cb342b0565eea276dc5ff06c97b6d
Jean A. Senellart <jean.senellart@systrangroup.com>
2018-10-10 15:32:45 +0200

    update python package to 10.9 since minimal version requirement is set anyway to 10.9 in setup.py.

--- python/make_py_wheel_mac.sh
+++ python/make_py_wheel_mac.sh
@@ -80,11 +80,11 @@ build() {
   mkdir -p dist/delocated_wheel
   curl -L -O https://bootstrap.pypa.io/get-pip.py
 
-  build_python 2.7 https://www.python.org/ftp/python/2.7.15/python-2.7.15-macosx10.6.pkg
+  build_python 2.7 https://www.python.org/ftp/python/2.7.15/python-2.7.15-macosx10.9.pkg
   build_python 3.4 https://www.python.org/ftp/python/3.4.4/python-3.4.4-macosx10.6.pkg
   build_python 3.5 https://www.python.org/ftp/python/3.5.4/python-3.5.4-macosx10.6.pkg
-  build_python 3.6 https://www.python.org/ftp/python/3.6.6/python-3.6.6-macosx10.6.pkg
-  build_python 3.7 https://www.python.org/ftp/python/3.7.0/python-3.7.0-macosx10.6.pkg
+  build_python 3.6 https://www.python.org/ftp/python/3.6.6/python-3.6.6-macosx10.9.pkg
+  build_python 3.7 https://www.python.org/ftp/python/3.7.0/python-3.7.0-macosx10.9.pkg
 
   cd ..
 
